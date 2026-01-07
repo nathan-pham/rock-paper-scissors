f682d8f9ae530de6b0bb63435948b6b29833534e
nathan-pham <157486459+nathan-pham@users.noreply.github.com>
2026-01-07 12:58:50 +1100

    Write choice functions

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,23 @@
-console.log("Hello World!")
+function getComputerChoice() {
+
+    val = Math.random() * 3;
+    
+    if (val <= 1) {
+        return "rock";
+    }
+    else if (val <= 2 && val > 1) {
+        return "paper";
+    }
+    else if (val <= 3 && val > 2) {
+        return "scissors";
+    }
+
+}
+
+console.log(getComputerChoice());
+
+function getHumanChoice() {
+    return prompt("Rock, paper, or scissors?");
+}
+
+console.log(getHumanChoice());
